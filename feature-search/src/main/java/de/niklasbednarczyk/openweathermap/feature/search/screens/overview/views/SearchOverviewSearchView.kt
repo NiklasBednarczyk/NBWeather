86b401cb46304f0ba8c02df1923430fbff434c1a
NiklasBednarczyk <54103393+NiklasBednarczyk@users.noreply.github.com>
2022-10-21 23:32:48 +0200

    updated todos (#10)

--- feature-search/src/main/java/de/niklasbednarczyk/openweathermap/feature/search/screens/overview/views/SearchOverviewSearchView.kt
+++ feature-search/src/main/java/de/niklasbednarczyk/openweathermap/feature/search/screens/overview/views/SearchOverviewSearchView.kt
@@ -30,7 +30,8 @@ fun SearchOverviewSearchView(
     navigateToLocation: (Double?, Double?) -> Unit
 ) {
 
-    //TODO (#10) Search Item window insets clickable weird + also check for ime changing height
+    //TODO (#10) Search Item window insets navigation bar horizontal weird => WindowInset
+    //TODO (#10) IME makes content smaller => WindowInsets (also maybe delete NoResultsView?)
 
     ResourceView(
         resource = searchedLocationsResource,
