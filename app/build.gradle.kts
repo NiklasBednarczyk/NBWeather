e0c511d5499c9e118ff05147a6c68b6bf8ca2da9
NiklasBednarczyk <54103393+NiklasBednarczyk@users.noreply.github.com>
2023-02-05 20:57:41 +0100

    release 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     }
     defaultConfig {
         applicationId = "de.niklasbednarczyk.nbweather"
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
