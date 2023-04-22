cc29bf8ea89414ff57e7c88cd9a82ee222e6a12e
NiklasBednarczyk <54103393+NiklasBednarczyk@users.noreply.github.com>
2023-04-22 15:29:57 +0200

    enabled crashlytics only for releases (#6)

--- app/src/main/java/de/niklasbednarczyk/nbweather/NBApplication.kt
+++ app/src/main/java/de/niklasbednarczyk/nbweather/NBApplication.kt
@@ -1,6 +1,7 @@
 package de.niklasbednarczyk.nbweather
 
 import android.app.Application
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -13,6 +14,7 @@ class NBApplication : Application() {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
+        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(!BuildConfig.DEBUG)
     }
 
 }
