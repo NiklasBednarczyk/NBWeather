b2d94e99336b908a48e784febda56a4750039cb2
NiklasBednarczyk <54103393+NiklasBednarczyk@users.noreply.github.com>
2023-05-14 20:02:38 +0200

    release 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
     }
     defaultConfig {
         applicationId = "de.niklasbednarczyk.nbweather"
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.1.0"
 
         vectorDrawables {
             useSupportLibrary = true
